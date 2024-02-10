a783dc5764cdb0e149d457273e6901912c26f9e0
NoobMahbub <Mahbubbengali@gmail.com>
2024-02-10 17:21:34 +0600

    RoundedCornerShape of bg using clip modifier

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
@@ -32,8 +33,11 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.lemonade.ui.theme.LemonadeTheme
 
 
@@ -75,19 +79,32 @@ fun Lemonade(name: String, modifier: Modifier = Modifier) {
                     verticalArrangement = Arrangement.Center,
                     modifier = Modifier.fillMaxSize()
                 ){
-                    Text(text = "lemon_select")
+
                     Spacer(modifier = Modifier.height(32.dp))
                     Image(
                         painter = painterResource(R.drawable.lemon_tree),
                         contentDescription = ("lemon_tree_content_description"),
                         modifier = Modifier
                             .wrapContentSize()
+                            .clip(RoundedCornerShape(percent = 20))
                             .background(
                                 color = Color(0xFFC8E6C9)
+                            )
+                            .padding(
+                                horizontal = 26.dp,
+                                vertical = 15.dp,
+
                             )
                             .clickable {
                                 currentStep = 2
                             }
+                    )
+
+                    Text(
+                        text = "Tap the lemon tree to select a lemon",
+                        fontSize = 20.sp,
+                        modifier = modifier
+                            .padding(15.dp)
                     )
                 }
             }
