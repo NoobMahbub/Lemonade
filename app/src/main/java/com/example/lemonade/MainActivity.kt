fc1c67c560d59d091979b4fda36769ede1c989d2
NoobMahbub <Mahbubbengali@gmail.com>
2024-02-10 14:23:49 +0600

    added clickable behaviour

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -13,20 +14,31 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.lemonade.ui.theme.LemonadeTheme
+
+
+
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -47,35 +59,55 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Lemonade(name: String, modifier: Modifier = Modifier) {
-    Column (
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp), // Add padding for better appearance
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+    // Current step the app is displaying (remember allows the state to be retained
+    // across recompositions).
+    var currentStep by remember { mutableIntStateOf(1) }
+
+    // A surface container using the 'background' color from the theme
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
     ) {
-
-        Box(
-            modifier = Modifier
-                .clip(RoundedCornerShape(percent = 50))
-                .background(
-                    color = Color(0xFFC8E6C9)
-                )
-        ) {
-            Image(
-                painter = painterResource(id = R.drawable.lemon_tree),
-                contentDescription = R.drawable.lemon_tree.toString(),
-
-
-                )
+        when (currentStep) {
+            1 -> {
+                Column (
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier.fillMaxSize()
+                ){
+                    Text(text = "lemon_select")
+                    Spacer(modifier = Modifier.height(32.dp))
+                    Image(
+                        painter = painterResource(R.drawable.lemon_tree),
+                        contentDescription = ("lemon_tree_content_description"),
+                        modifier = Modifier
+                            .wrapContentSize()
+                            .clickable {
+                                currentStep = 2
+                            }
+                    )
+                }
+            }
+            2 -> {
+                Column (
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier.fillMaxSize()
+                ){
+                    Text(text = "lemon_squeeze")
+                    Spacer(modifier = Modifier.height(32
+                        .dp))
+                    Image(
+                        painter = painterResource(R.drawable.lemon_squeeze),
+                        contentDescription = "lemon_tree_content_description",
+                        modifier = Modifier.wrapContentSize()
+                    )
+                }
+            }
         }
-
-        Text(
-            text = "Hello $name!",
-            modifier = modifier
-        )
     }
 }
+
 
 @Preview(
     showBackground = true,
